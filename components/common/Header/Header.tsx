a2f94ac7c1cab3d0f852cff7df1639b1cabd7c92
Mostafa Waleeed <mostafawaleed206@gmail.com>
2023-01-12 07:59:52 +0200

    📦Code Refactoring: delete unused variables & code

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,14 +1,9 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { Brand } from 'components/ui';
-import { useState } from 'react';
-import { List } from 'components/icons';
-import { lang } from 'lib';
 
 export default function Header() {
-  const data = lang();
   const router = useRouter();
-  const [toggle, setToggle] = useState(true);
   const { locale } = useRouter();
 
   return (
@@ -19,26 +14,6 @@ export default function Header() {
             <Link href="/">
               <Brand />
             </Link>
-            <nav
-              aria-label="primary"
-              tabIndex={-1}
-              className={`md:flex p-3 lg:p-0 absolute top-full right-0 md:static md:h-fit text-lg bg-yellow-400 md:bg-transparent rounded-lg ${
-                toggle ? 'hidden' : 'flex'
-              }`}
-            >
-              <ul
-                className="flex flex-col md:flex-row text-lg text-black md:text-white capitalize font-medium divide-y-2 md:divide-y-0 divide-white"
-                role="list"
-              >
-                {/* {Object.keys({ ...data?.header }).map((item) => {
-                  return (
-                    <li key={item} className="p-4">
-                      <Link href={data.header[item]}>{item}</Link>
-                    </li>
-                  );
-                })} */}
-              </ul>
-            </nav>
             <div className="flex gap-3 items-center">
               <Link
                 locale={locale === 'en' ? 'ar' : 'en'}
@@ -47,14 +22,6 @@ export default function Header() {
               >
                 {locale === 'en' ? 'ar' : 'en'}
               </Link>
-              {/* <button
-                type="button"
-                className="md:hidden uppercase w-12 h-12 font-bold bg-green-600 rounded-lg grid place-content-center hover:ring-2 ring-yellow-400 transition-all"
-                onClick={() => setToggle((x) => !x)}
-                aria-label="Toggle Menu"
-              >
-                <List />
-              </button> */}
             </div>
           </div>
         </div>
