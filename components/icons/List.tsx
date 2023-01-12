af17ce5910471233b6a10f95288c00cd778c07b0
Mostafa Waleeed <mostafawaleed206@gmail.com>
2023-01-12 07:54:34 +0200

    📦 Code Refactoring: DOM property usage

--- components/icons/List.tsx
+++ components/icons/List.tsx
@@ -11,7 +11,7 @@ export default function List({ ...props }) {
       {...props}
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
       />
     </svg>
