b06215603dc851a7c84a85528c08ad1ec846b56f
Mostafa Waleeed <mostafawaleed206@proton.me>
2023-12-02 17:55:17 +0200

    Delete unused configurations

--- next.config.js
+++ next.config.js
@@ -1,21 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  unstable_runtimeJS: false,
-  reactStrictMode: true,
-  swcMinify: true,
-
   images: {
-    domains: ["static.abstractapi.com"],
+    domains: ['static.abstractapi.com']
   },
   i18n: {
     // These are all the locales you want to support in
     // your application
-    locales: ["en", "ar"],
+    locales: ['en', 'ar'],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: "ar",
-    localeDetection: false,
-  },
+    defaultLocale: 'ar',
+    localeDetection: false
+  }
 };
 
 module.exports = nextConfig;
