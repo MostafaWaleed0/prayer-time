dd3b862ba5bf48fd1f838e8f70d7b4c7f3679df7
Mostafa Waleeed <mostafawaleed206@proton.me>
2024-02-15 01:53:28 +0200

    Improve readability by using more descriptive variable names in `secondsToHm` function

--- app/[lang]/utils/seconds-to-hm.ts
+++ app/[lang]/utils/seconds-to-hm.ts
@@ -1,12 +1,13 @@
-export const secondsToHm = (d: number) => {
-  d = Number(d);
-  const h = Math.floor(d / 3600);
-  const m = Math.floor((d % 3600) / 60);
+export const secondsToHm = (totalSeconds: number) => {
+  totalSeconds = Number(totalSeconds);
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
 
-  const shortHour = h % 12 ?? 12;
+  const shortHour = hours % 12 ?? 12;
 
-  const dateDisplay = `${shortHour < 10 ? `0${shortHour}` : `${shortHour}`}:${m < 10 ? `0${m}` : `${m}`} ${
-    h <= 12 ? 'AM' : 'PM'
+  const timeDisplay = `${shortHour < 10 ? `0${shortHour}` : `${shortHour}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`} ${
+    hours <= 12 ? 'AM' : 'PM'
   }`;
-  return dateDisplay;
+
+  return timeDisplay;
 };
