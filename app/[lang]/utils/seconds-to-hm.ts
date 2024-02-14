7033eff14f46c8dd45defd170d9b5e7c887490ce
Mostafa Waleeed <mostafawaleed206@proton.me>
2024-02-15 01:34:17 +0200

    Replace `||` with `??` for default value assignment in shortHour calculation

--- app/[lang]/utils/seconds-to-hm.ts
+++ app/[lang]/utils/seconds-to-hm.ts
@@ -3,7 +3,7 @@ export const secondsToHm = (d: number) => {
   const h = Math.floor(d / 3600);
   const m = Math.floor((d % 3600) / 60);
 
-  const shortHour = h % 12 || 12;
+  const shortHour = h % 12 ?? 12;
 
   const dateDisplay = `${shortHour < 10 ? `0${shortHour}` : `${shortHour}`}:${m < 10 ? `0${m}` : `${m}`} ${
     h <= 12 ? 'AM' : 'PM'
