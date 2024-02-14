b024d21161277fea73d97f8a68f900b4c08397ae
Mostafa Waleeed <mostafawaleed206@proton.me>
2024-02-15 01:30:45 +0200

    Refactor code to utilize optional chaining operator for null checks

--- app/[lang]/components/prayer-view.tsx
+++ app/[lang]/components/prayer-view.tsx
@@ -15,58 +15,55 @@ export default function PrayerView({ prayers, geolocation, dict, locale }: Praye
   return (
     <div className="grid lg:grid-cols-2 gap-20 items-center lg:gap-40 space-y-4 font-medium w-full">
       <ol role="list" className="space-y-2 order-1 lg:-order-1">
-        {prayers &&
-          prayers.map(({ name, style, icon, prayerHour }, i: number) => {
-            const secondsOfTheDay = convertToSeconds('24:00:00');
-            const secondsOfTheUpcomingPrayer = convertToSeconds(prayerHour);
-            const secondsFromTheBeginningOfTheDayToTheCurrentMoment = convertToSeconds(
-              currentTime.toLocaleTimeString('en', {
-                hour12: false,
-                timeZone: geolocation.timezone
-              })
-            );
+        {prayers?.map(({ name, style, icon, prayerHour }, i: number) => {
+          const secondsOfTheDay = convertToSeconds('24:00:00');
+          const secondsOfTheUpcomingPrayer = convertToSeconds(prayerHour);
+          const secondsFromTheBeginningOfTheDayToTheCurrentMoment = convertToSeconds(
+            currentTime.toLocaleTimeString('en', {
+              hour12: false,
+              timeZone: geolocation.timezone
+            })
+          );
 
-            const theUpcomingPrayerOfFajrPrayer =
-              secondsOfTheDay - secondsFromTheBeginningOfTheDayToTheCurrentMoment + convertToSeconds(prayers[0].prayerHour);
-            const secondsOfTheLastPrayer = convertToSeconds(prayers[prayers.length - 1].prayerHour);
+          const theUpcomingPrayerOfFajrPrayer =
+            secondsOfTheDay - secondsFromTheBeginningOfTheDayToTheCurrentMoment + convertToSeconds(prayers[0].prayerHour);
+          const secondsOfTheLastPrayer = convertToSeconds(prayers[prayers.length - 1].prayerHour);
 
-            const fajrPrayerTheNextDay = secondsOfTheLastPrayer < secondsFromTheBeginningOfTheDayToTheCurrentMoment;
-            const upcomingPrayers = secondsOfTheUpcomingPrayer > secondsFromTheBeginningOfTheDayToTheCurrentMoment;
-            const remainingTimeForUpcomingPrayer = secondsOfTheUpcomingPrayer - secondsFromTheBeginningOfTheDayToTheCurrentMoment;
+          const fajrPrayerTheNextDay = secondsOfTheLastPrayer < secondsFromTheBeginningOfTheDayToTheCurrentMoment;
+          const upcomingPrayers = secondsOfTheUpcomingPrayer > secondsFromTheBeginningOfTheDayToTheCurrentMoment;
+          const remainingTimeForUpcomingPrayer = secondsOfTheUpcomingPrayer - secondsFromTheBeginningOfTheDayToTheCurrentMoment;
 
-            arrayOfRemainingTimeForUpcomingPrayer.push(remainingTimeForUpcomingPrayer);
-            arrayOfUpcomingPrayers.push(upcomingPrayers);
+          arrayOfRemainingTimeForUpcomingPrayer.push(remainingTimeForUpcomingPrayer);
+          arrayOfUpcomingPrayers.push(upcomingPrayers);
 
-            firstUpcomingPrayer = arrayOfUpcomingPrayers.findIndex((x) => x === true);
+          firstUpcomingPrayer = arrayOfUpcomingPrayers.findIndex((x) => x === true);
 
-            const styleOfUpcomingPrayer = `absolute top-2/4 hidden lg:block ${
-              locale === 'en' ? 'left-[calc(100%+4rem)]' : 'right-[calc(100%-1rem)]'
-            } rounded-md px-3 py-6 ${style} -translate-x-1/2 -translate-y-1/2`;
+          const styleOfUpcomingPrayer = `absolute top-2/4 hidden lg:block ${
+            locale === 'en' ? 'left-[calc(100%+4rem)]' : 'right-[calc(100%-1rem)]'
+          } rounded-md px-3 py-6 ${style} -translate-x-1/2 -translate-y-1/2`;
 
-            return (
-              <li key={name} className={`p-4 rounded-[10px] relative ${style}`}>
-                <div className="flex items-center justify-between" dir="ltr">
-                  <div className="flex items-center justify-center space-x-5">
-                    {icon}
-                    <div className="px-4">{name}</div>
-                  </div>
-                  <time dateTime={prayerHour}>{secondsToHm(secondsOfTheUpcomingPrayer)}</time>
+          return (
+            <li key={name} className={`p-4 rounded-[10px] relative ${style}`}>
+              <div className="flex items-center justify-between" dir="ltr">
+                <div className="flex items-center justify-center space-x-5">
+                  {icon}
+                  <div className="px-4">{name}</div>
                 </div>
-                {arrayOfUpcomingPrayers[arrayOfUpcomingPrayers.indexOf(true) === i ? firstUpcomingPrayer : undefined!] ? (
-                  <div className={styleOfUpcomingPrayer}>
-                    <Suspense fallback={null}>
-                      {secondsToHms(arrayOfRemainingTimeForUpcomingPrayer[firstUpcomingPrayer])}
-                    </Suspense>
-                    <div></div>
-                  </div>
-                ) : i === 0 && fajrPrayerTheNextDay ? (
-                  <div className={styleOfUpcomingPrayer}>
-                    <Suspense fallback={null}>{secondsToHms(theUpcomingPrayerOfFajrPrayer)}</Suspense>
-                  </div>
-                ) : null}
-              </li>
-            );
-          })}
+                <time dateTime={prayerHour}>{secondsToHm(secondsOfTheUpcomingPrayer)}</time>
+              </div>
+              {arrayOfUpcomingPrayers[arrayOfUpcomingPrayers.indexOf(true) === i ? firstUpcomingPrayer : undefined!] ? (
+                <div className={styleOfUpcomingPrayer}>
+                  <Suspense fallback={null}>{secondsToHms(arrayOfRemainingTimeForUpcomingPrayer[firstUpcomingPrayer])}</Suspense>
+                  <div></div>
+                </div>
+              ) : i === 0 && fajrPrayerTheNextDay ? (
+                <div className={styleOfUpcomingPrayer}>
+                  <Suspense fallback={null}>{secondsToHms(theUpcomingPrayerOfFajrPrayer)}</Suspense>
+                </div>
+              ) : null}
+            </li>
+          );
+        })}
       </ol>
       <div>
         <Image
