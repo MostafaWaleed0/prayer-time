f09d3ab5af26469fa5c4e1081816a629548a73d7
Mostafa Waleeed <mostafawaleed206@proton.me>
2025-01-18 19:29:41 +0200

    remove unstable_cache usage and update async functions

--- app/[lang]/lib/api.ts
+++ app/[lang]/lib/api.ts
@@ -1,6 +1,5 @@
 'use server';
 
-import { unstable_cache as cache } from 'next/cache';
 import { headers } from 'next/headers';
 import { GeolocationProps } from './type';
 
@@ -11,7 +10,7 @@ export async function loadGeolocation() {
   try {
     // IMPORTANT: When using this code locally, ensure to replace the IP address in the API URL from
     // https://ipapi.co/${ip}/json to https://ipapi.co/json. Failure to do so will result in errors.
-    let res = await fetch(`https://ipapi.co/${ip}/json`, { next: { revalidate: 3600 } });
+    let res = await fetch(`https://ipapi.co/${ip}/json`);
     return res.json();
   } catch (error) {
     console.error('Error fetching geolocation:', error);
@@ -19,42 +18,30 @@ export async function loadGeolocation() {
   }
 }
 
-export const getTimingsByCity = cache(
-  async (geolocation: GeolocationProps) => {
-    try {
-      let res = await fetch(
-        `https://api.aladhan.com/v1/timingsByCity?city=${encodeURIComponent(geolocation.city)}&country=${encodeURIComponent(
-          geolocation.country_name
-        )}`
-      );
+export const getTimingsByCity = async (geolocation: GeolocationProps) => {
+  try {
+    let res = await fetch(
+      `https://api.aladhan.com/v1/timingsByCity?city=${encodeURIComponent(geolocation.city)}&country=${encodeURIComponent(
+        geolocation.country_name
+      )}`
+    );
 
-      return res.json();
-    } catch {
-      throw new Error('Error fetching timings');
-    }
-  },
-  ['timings-by-city'],
-  {
-    revalidate: 1800
+    return res.json();
+  } catch {
+    throw new Error('Error fetching timings');
   }
-);
+};
 
-export const getCalendarByCity = cache(
-  async (geolocation: GeolocationProps) => {
-    try {
-      let res = await fetch(
-        `http://api.aladhan.com/v1/calendarByCity?city=${encodeURIComponent(geolocation.city)}&country=${encodeURIComponent(
-          geolocation.country_name
-        )}`
-      );
+export const getCalendarByCity = async (geolocation: GeolocationProps) => {
+  try {
+    let res = await fetch(
+      `http://api.aladhan.com/v1/calendarByCity?city=${encodeURIComponent(geolocation.city)}&country=${encodeURIComponent(
+        geolocation.country_name
+      )}`
+    );
 
-      return res.json();
-    } catch {
-      throw new Error('Error fetching calendar');
-    }
-  },
-  ['calendar'],
-  {
-    revalidate: 3600
+    return res.json();
+  } catch {
+    throw new Error('Error fetching calendar');
   }
-);
+};
